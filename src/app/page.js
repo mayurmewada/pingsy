95a570e690a5d520fad5b492e19138a183a20a93
mayurmewada <mayurmewada00@gmail.com>
2025-06-25 00:44:13 +0530

    feat: chat section UI

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
                     </h1>
                     <input className="bg-[background:var(--surface)] rounded-[12px] w-full h-[40px] text-[color:var(--textlight)] px-4" placeholder="Search" type="text" />
                 </div>
-                <div className="overflow-y-scroll h-[calc(100vh-127px)] p-3">
+                <div className="overflow-y-scroll h-[calc(100vh-135px)] p-3">
                     <div className="p-3 flex items-center gap-3">
                         <div className="min-w-[50px] max-w-[50px] min-h-[50px] max-h-[50px] rounded-[50%] bg-[#333333]"></div>
                         <div className="text-[color:var(--textlight)] w-full flex flex-col justify-center gap-[2px]">
@@ -43,10 +43,23 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className="max-w-[70%] w-full border-l-[1px] border-l-[border-left-color:var(--surface)]">
-                <p className="text-[color:var(--textlight)]">hello world</p>
-                <div className="w-[400px] h-[200px] bg-[background:var(--surface)] text-[color:var(--textlight)]">---</div>
-                <div className="w-[100px] h-[50px] bg-[background:var(--primary)] text-[color:var(--textdark)]">---</div>
+            <div className="max-w-[70%] w-full border-l-[1px] border-l-[border-left-color:var(--surface)] flex flex-col">
+                <div className="w-full px-5 py-4 text-[color:var(--textdark)]">
+                    <div className="flex items-center gap-3">
+                        <div className="min-w-[50px] max-w-[50px] min-h-[50px] max-h-[50px] rounded-[50%] bg-[#333333]"></div>
+                        <div className="text-[color:var(--textlight)] w-full flex flex-col justify-center gap-[2px]">
+                            <div className="flex justify-between items-baseline">
+                                <span className="leading-[1] text-[18px]">username</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-[400px] h-[200px] border-y-[1px] border-y-[border-left-color:var(--surface)] h-full w-full relative">
+                  <Image className="absolute w-full h-full object-cover opacity-[50%] inset-[0] invert-[1]" src={"/images/chatbg.png"} alt="chat-background-vector" width={800} height={600} />
+                </div>
+                <div className="w-full px-5 py-4 text-[color:var(--textdark)]">
+                    <input className="bg-[background:var(--surface)] rounded-[12px] w-full h-[40px] text-[color:var(--textlight)] px-4" placeholder="Type Message" type="text" />
+                </div>
             </div>
         </main>
     );
